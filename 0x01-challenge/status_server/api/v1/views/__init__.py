7c99c9f676328fbf62c1b6edf88feedcf4db61ae
meriembenayad <meriem.benayad@gmail.com>
2023-12-11 13:54:24 +0100

    Task 0: Server Status

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Views module
 """
-from api.v1.views.index import index
+from api.v1.views import index
 from flask import Blueprint
+
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
